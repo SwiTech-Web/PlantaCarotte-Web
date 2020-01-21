239258d281601c1476fa39a4725861fad48784c0
Ilan BENAYOUN <ilan77420@hotmail.fr>
2020-01-21 23:13:21 +0100

    Hotfix - Recode + Firebase environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,15 @@
+import 'firebase/firestore';
+
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBzbAio1QXY9GvrXN5XPOcnNtZVN_TA5V4",
-    authDomain: "plantacarotte.firebaseapp.com",
-    databaseURL: "https://plantacarotte.firebaseio.com",
-    projectId: "plantacarotte",
-    storageBucket: "plantacarotte.appspot.com",
-    messagingSenderId: "122304205859",
-    appId: "1:122304205859:web:664842da6bbb2bdb1e4d96",
-    measurementId: "G-1HS3LNWN13"
+    apiKey: 'AIzaSyBzbAio1QXY9GvrXN5XPOcnNtZVN_TA5V4',
+    authDomain: 'plantacarotte.firebaseapp.com',
+    databaseURL: 'https://plantacarotte.firebaseio.com',
+    projectId: 'plantacarotte',
+    storageBucket: 'plantacarotte.appspot.com',
+    messagingSenderId: '122304205859',
+    appId: '1:122304205859:web:664842da6bbb2bdb1e4d96',
+    measurementId: 'G-1HS3LNWN13'
   }
 };
